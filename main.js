000a801a2b5a1e4a74137fa732926a8c79a94933
Juan Mora <juan.mora@gl.local>
2017-09-23 18:54:49 -0300

    Se deja funcional el chat persona a persona

--- main.js
+++ main.js
@@ -3,20 +3,16 @@ let app = express();
 
 let io = require('socket.io').listen(app.listen(8080));
 
-// io.sockets.on('connection', function (socket) {
-//     socket.emit('message', { datos: 'He recibido estos datos desde el servidor' });
-//     socket.on('send', function (data) {
-//         io.sockets.emit('message', {datos: data});
-//     });
-// });
+var people = {};
 
 io.sockets.on('connection', function (socket) {
 
-    socket.on('new_message', function(data) {
-        console.log(data);
-        socket.device_id = data.device_id;
-        socket.agent_id = data.agent_id;
-        socket.broadcast.to(socket.devide_id).emit('my message', 'holowows');
+    people[socket.handshake.query.name] = socket.id;
+
+    console.log(people);
+
+    socket.on('send', function (destinatario, msg) {
+        io.to(people[destinatario]).emit('message', msg);
     });
 
 });
