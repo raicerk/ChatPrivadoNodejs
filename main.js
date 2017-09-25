2720a94d888932232252ba2d75ac63e450780e7f
Juan Mora <juan.mora@glgroup.cl>
2017-09-24 22:17:39 -0300

    Se agrega cantidad de usuarios conectados

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ io.sockets.on('connection', function (socket) {
     people[nombre] = socket.id;
 
     io.emit('lista', people);
+    io.emit('CantidadConectados',Object.keys(people).length);
 
     socket.on('send', function (destinatario, msg) {
         io.to(people[destinatario]).emit('message', msg);
@@ -26,6 +27,7 @@ io.sockets.on('connection', function (socket) {
         console.log(`Se desconecto ${nombre}`);
         delete people[nombre];
         io.emit('lista', people);
+        io.emit('CantidadConectados',Object.keys(people).length);
     });
 
     console.log('--------------------------------');
