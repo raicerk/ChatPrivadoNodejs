e9264fae35bb9d110dc1e029ac4e1da959a3b18e
Juan Mora <raicerk@gmail.com>
2017-09-24 23:07:41 -0300

    Actualización de los datos de conexión

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    cadena: 'workstation id=server;packet size=4096;user id=sa;data source=192.168.0.250;initial catalog=GL_Intranet;password=ToQNGL391#.',
+    cadena: 'workstation id=server;packet size=4096;user id=sa;data source=192.168.0.xxx;initial catalog=NombreDeTuBaseDeDatos;password=TuContraseñaPro',
     puerto: 8080
 };
